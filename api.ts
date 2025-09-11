3d7c69c56efd08de3e1107f66d3fcd9e169d3bf0
Suryansh-Singhai <93902422+Suryansh-Singhai@users.noreply.github.com>
2025-09-11 07:51:31 +0000

    Day4

--- api.ts
+++ api.ts
@@ -7,3 +7,11 @@ export async function fetchData(id: string): Promise<string>{
     }
     return response.json();
 }
+
+//https://jsonplaceholder.typicode.com/posts/1
+
+export async function fetchData2(id: string): Promise<string> {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+    if(!response.ok) throw new Error(`Error status: ${response.status}`);
+    return response.json();
+}
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,1 @@
-require('jest-fetch-mock');
+require('jest-fetch-mock').enableFetchMocks();
