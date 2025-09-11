2f6faea32f550695c133b5153de5486dd3167782
Suryansh-Singhai <93902422+Suryansh-Singhai@users.noreply.github.com>
2025-09-11 08:25:52 +0000

    removed redundant code

--- assignment2.ts
+++ assignment2.ts
@@ -5,25 +5,21 @@ interface Book{
     isAvailable: boolean;
 }
 
-class PrintBook implements Book{
-    title: string;
-    author: string;
-    pages: number;
-    isAvailable: boolean;
-    constructor(title: string, author: string, pages: number, isAvailable: boolean){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.isAvailable = isAvailable;
-    }
-}
-
 function printBookInfo(book: Book): void {
     console.log(`title: ${book.title}, author: ${book.author}, pages: ${book.pages}, isAvailable: ${book.isAvailable}`)
 }
 
-const b1: Book = new PrintBook("Harry Potter", "J K Rowling", 300, true);
-const b2: Book = new PrintBook("MyBook", "Suryansh", 250, false);
+const b1: Book ={
+    title: "Harry Potter", 
+    author: "J K Rowling", 
+    pages: 300, 
+    isAvailable: true};
+    
+const b2: Book = {
+    title: "MyBook", 
+    author: "Suryansh", 
+    pages: 250, 
+    isAvailable: false};
 
 printBookInfo(b1); 
 printBookInfo(b2);
